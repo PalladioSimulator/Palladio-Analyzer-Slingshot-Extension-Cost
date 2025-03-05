0695b32399e2c125ac9247002790e228bbd686ff
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2025-03-05 15:05:44 +0100

    ensure presence of stereotypes by reloading.

--- src/org/palladiosimulator/analyzer/slingshot/cost/CostMonitorBehavior.java
+++ src/org/palladiosimulator/analyzer/slingshot/cost/CostMonitorBehavior.java
@@ -1,5 +1,6 @@
 package org.palladiosimulator.analyzer.slingshot.cost;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -7,6 +8,9 @@ import java.util.Optional;
 import javax.inject.Inject;
 
 import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.palladiosimulator.analyzer.slingshot.common.annotations.Nullable;
 import org.palladiosimulator.analyzer.slingshot.common.events.AbstractSimulationEvent;
 import org.palladiosimulator.analyzer.slingshot.core.extension.SimulationBehaviorExtension;
@@ -23,9 +27,13 @@ import org.palladiosimulator.analyzer.slingshot.monitor.data.events.modelvisited
 import org.palladiosimulator.analyzer.slingshot.monitor.data.events.modelvisited.MonitorModelVisited;
 import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPoint;
 import org.palladiosimulator.edp2.util.MetricDescriptionUtility;
+import org.palladiosimulator.mdsdprofiles.api.ProfileAPI;
 import org.palladiosimulator.mdsdprofiles.api.StereotypeAPI;
 import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 import org.palladiosimulator.monitorrepository.MeasurementSpecification;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentFactory;
+import org.palladiosimulator.pcm.util.PcmResourceImpl;
 import org.palladiosimulator.pcmmeasuringpoint.ResourceContainerMeasuringPoint;
 import org.palladiosimulator.probeframework.calculator.Calculator;
 import org.palladiosimulator.probeframework.calculator.DefaultCalculatorProbeSets;
@@ -75,28 +83,38 @@ public class CostMonitorBehavior implements SimulationBehaviorExtension {
 				&& MetricDescriptionUtility.metricDescriptionIdsEqual(spec.getMetricDescription(),
 						MetricDescriptionConstants.COST_OF_RESOURCE_CONTAINERS)) {
 
-			final Optional<ElasticInfrastructureCfg>  eiCfg = semanticConfiguration.getTargetCfgs().stream()
+			final Optional<ElasticInfrastructureCfg> eiCfg = semanticConfiguration.getTargetCfgs().stream()
 					.filter(cfg -> cfg instanceof final ElasticInfrastructureCfg c)
 					.map(cfg -> (ElasticInfrastructureCfg) cfg)
-					.filter(cfg -> cfg.getUnit().equals(rcmp.getResourceContainer()))
-					.findFirst();
+					.filter(cfg -> cfg.getUnit().equals(rcmp.getResourceContainer())).findFirst();
 
 			if (eiCfg.isEmpty()) {
-				LOGGER.debug(String.format(
+				LOGGER.info(String.format(
 						"Not registering Calculator for %s, no ElasticInfrasturctureConfiguration with matching Unit.",
 						rcmp.getStringRepresentation()));
 				return Result.empty();
 			}
-			
+
+			// The Palladio Bench has problems with loading and resolving the cost
+			// Stereotypes, unless the {@code apply} operation have been called before
+			// loading the resources. Usually, the Profile is available, but the Stereotypes
+			// are missing. If this is the case, manually call apply on another model and
+			// then reload the actual model.
+			if (!ProfileAPI.getAppliedProfiles(eiCfg.get().getResourceEnvironment().eResource()).isEmpty()
+					&& StereotypeAPI.getAppliedStereotypes(eiCfg.get().getUnit()).isEmpty()) {
+				LOGGER.info("A Profile is applied to Resource Environment, but the Stereotypes are missing. "
+						+ "Attempting to fix this by Appling Profiles to a fake model and reloading the original.");
+				applyStereotypeFake();
+				reloadResEnv(eiCfg.get().getResourceEnvironment().eResource());
+			}
+
 			if (StereotypeAPI.getAppliedStereotypes(eiCfg.get().getUnit()).isEmpty()) {
-				LOGGER.debug(String.format(
+				LOGGER.info(String.format(
 						"Not registering Calculator for %s, because there are no Costs defined for the Resource Container.",
 						rcmp.getStringRepresentation()));
 				return Result.empty();
 			} else {
-				LOGGER.debug(String.format(
-						"Registering Calculator for %s.",
-						rcmp.getStringRepresentation()));
+				LOGGER.info(String.format("Registering Calculator for %s.", rcmp.getStringRepresentation()));
 			}
 
 			final Calculator calculator = setupCalculator(rcmp, calculatorFactory, eiCfg.get());
@@ -108,6 +126,32 @@ public class CostMonitorBehavior implements SimulationBehaviorExtension {
 
 		}
 		return Result.empty();
+	}
+
+	/**
+	 * Reload the given resource.
+	 * 
+	 * @param res resource to reload.
+	 */
+	private void reloadResEnv(final Resource res) {
+		res.unload();
+		try {
+			res.load(((XMLResource) res).getDefaultLoadOptions());
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create a resource and a resource environment model and apply the cost profile
+	 * stereotype to get them loaded.
+	 */
+	private void applyStereotypeFake() {
+		final ResourceEnvironment fake = ResourceenvironmentFactory.eINSTANCE.createResourceEnvironment();
+		final Resource fakeRes = new PcmResourceImpl(URI.createFileURI(java.lang.System.getProperty("java.io.tmpdir")));
+		fakeRes.getContents().add(fake);
+		ProfileAPI.applyProfile(fakeRes, "Cost");
+		StereotypeAPI.applyStereotype(fake, "CostReport");
 	}
 
 	@Subscribe
